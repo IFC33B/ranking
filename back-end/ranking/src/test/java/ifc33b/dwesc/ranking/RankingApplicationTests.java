278aa2a0b5f266522326ab1f12a98f2b8d8c27b5
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-28 15:25:36 +0100

    create: back-end structure

---
+++ back-end/ranking/src/test/java/ifc33b/dwesc/ranking/RankingApplicationTests.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
