ea7d38631c599e76663bbd38cae404ab34911265
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-28 16:07:43 +0100

    create: GlobalExceptionHandler.java

---
+++ back-end/ranking/src/main/java/ifc33b/dwesc/ranking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package ifc33b.dwesc.ranking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    // Candidato no encontrado
+    public ResponseEntity<ErrorResponse> handleCandidatNotFoundException(CandidatNotFoundException ex) {
+        ErrorResponse error = ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
