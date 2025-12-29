9fa3568a78100c9a52390637e9fec56b62cf9fb6
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-29 15:42:30 +0100

    Fix(GlobalExceptionHandler): add @ExceptionHandler

--- back-end/ranking/src/main/java/ifc33b/dwesc/ranking/exception/GlobalExceptionHandler.java
+++ back-end/ranking/src/main/java/ifc33b/dwesc/ranking/exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package ifc33b.dwesc.ranking.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
     // Candidato no encontrado
+    @ExceptionHandler(CandidatNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCandidatNotFoundException(CandidatNotFoundException ex) {
         ErrorResponse error = ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
