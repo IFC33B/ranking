8fe1a416b2cacad9dd7a21dc3f284c296b4ce03a
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-29 16:27:29 +0100

    add(app.routes.ts): add main routes

--- front-end/ranking/src/app/app.routes.ts
+++ front-end/ranking/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LlistaCandidats } from './components/llista-candidats/llista-candidats';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/candidats', pathMatch: 'full' },
+    { path: 'candidats', component: LlistaCandidats },
+    // { path: 'formulari', component: FormulariProductes }
+];
