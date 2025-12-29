1fd47002f5e2858df8f8c7614a5ee537c48472d5
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-29 17:13:22 +0100

    create: formulari-candidats component

---
+++ front-end/ranking/src/app/components/formulari-candidats/formulari-candidats.ts
@@ -0,0 +1,52 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CandidatService } from '../../services/candidat.service';
+
+@Component({
+  selector: 'app-formulari-candidats',
+  imports: [],
+  templateUrl: './formulari-candidats.html',
+  styleUrl: './formulari-candidats.css',
+})
+export class FormulariCandidats implements OnInit {
+  carregant = signal(false);
+  error = signal<HttpErrorResponse | null>(null);
+
+  // Formulario reactivo
+  candidatForm!: FormGroup;
+
+  constructor(private candidatService: CandidatService) { }
+
+  ngOnInit(): void {
+    // Campos del formulario
+    this.candidatForm = new FormGroup({
+      nom: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
+  }
+
+  // Añadir candidato
+  afegirCandidats() {
+    this.carregant.set(true);
+    this.error.set(null);
+
+    if (this.candidatForm.valid) {
+
+      // Service
+      this.candidatService.createCandidat(this.candidatForm.value).subscribe({
+        next: (data) => {
+          this.carregant.set(false);
+          this.candidatForm.reset();
+        },
+
+        error: (err) => {
+          this.error.set(err);
+          this.carregant.set(false);
+          console.log(err);
+        }
+      })
+    } else {
+      alert("Datos inválidos")
+    }
+  }
+}
